fc89e0cb5b03bf150bddf3b5a90e8e0e6fec34da
aviboot92 <avkarra@gmail.com>
2019-09-05 17:06:00 +0530

    new todo working

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,20 @@ li.on("click", function () {
 
 const del = $(".del");
 del.on("click", function (event) {
-    $(this).parent().fadeOut(500,function(){
+    $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+const nTd = $("#new_todo");
+nTd.keypress(function (event) {
+    if ((event.charCode || event.which) === 13) {
+        if ($(this).val() !== "") {
+            const newTodo = $(this).val();
+            const newTdHtml = ('<li><span class="del">X</span> ' + newTodo + '</li>');
+            $("ul").append(newTdHtml);
+            console.log($(this));
+        }
+    }
 });
