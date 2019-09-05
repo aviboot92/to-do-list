52c27f6611668f70ed26e8a6ce2a08f96c699d5a
aviboot92 <avkarra@gmail.com>
2019-09-05 16:34:50 +0530

    x working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-alert("Connected");
+
+const li = $("li");
+li.on("click", function () {
+    $(this).toggleClass("done");
+    // alert("CLicked");
+});
+
+const del = $(".del");
+del.on("click", function (event) {
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
